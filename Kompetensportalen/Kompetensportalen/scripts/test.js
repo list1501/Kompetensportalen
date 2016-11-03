d8498c522055ccb2d60e4838a25d8e8097f6872c
SH1N081 <maca1503@student.miun.se>
2016-11-03 12:04:49 +0100

    Påbörjat javascript för att öppna provet i modal box

---
+++ Kompetensportalen/Kompetensportalen/scripts/test.js
@@ -0,0 +1,15 @@
+﻿var modal = document.getElementById("testModal");
+
+var startButton = document.getElementById("btnStartTest");
+
+var close = document.getElementsByClassName("close")[0];
+
+startButton.onclick = function()
+{
+    modal.style.display = "block";
+}
+
+close.onclick = function()
+{
+    modal.style.display = "none";
+}

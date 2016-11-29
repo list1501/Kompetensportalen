1d4cbff0bf68300c193d4f582c94413f757a9310
list1501 <list1501@student.miun.se>
2016-11-29 10:04:19 +0100

    serializingclass

---
+++ Kompetensportalen/Kompetensportalen/Serialize.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Kompetensportalen
+{
+    public static class Serialize
+    {
+
+        //Helper class to Serialize Class directly to String. Found on here: http://stackoverflow.com/questions/2434534/serialize-an-object-to-string
+        //Must be in a static class as it is to work with more than one type of object
+
+        public static string Serializing<T>(this T toSerialize)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var textWriter = new StringWriter();
+            xmlSerializer.Serialize(textWriter, toSerialize);
+            return textWriter.ToString();
+        }
+
+        public static T Deserialize<T>(this string toDeserialize)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var textReader = new StringReader(toDeserialize);
+            return (T)xmlSerializer.Deserialize(textReader);
+        }
+    }
+}
+}
